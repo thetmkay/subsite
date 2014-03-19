4d8fd64015000941bd3b648f0d8960d9c460ea40
George Nishimura <thetmkay@gmail.com>
2014-03-19 15:00:00 +0000

    design iteration 3

--- public/site.js
+++ public/site.js
@@ -1,29 +1,40 @@
 $(document).on('ready', function() {
 
+	// $("#videoModal").modal();
+
 	var videoHeight = "0px"
 	var videoRatio = 1.35;
 
+	var resizeVideo = function() {
+		var height = $(window).height();
+		var width = $(window).width();
+
+		$video = $("#videoModal iframe");
+
+		$video.attr("width", Math.floor(width*0.8));
+		$video.attr("height", Math.floor(height*0.8));
+	};
+
 	var setWindowSize = function() {
 		var height = $(window).height();
-		var mainHeight = Math.floor(height*0.9);
+		var mainHeight = Math.floor(height*0.8);
 		videoHeight = mainHeight + "px";
-		var subHeight = Math.ceil(height*0.1);
+		var subHeight = Math.ceil(height*0.2);
 		$("#logoPage").css({
-			'height': videoHeight
-		});
-		$("#logoPage div").css({
+			'height': videoHeight,
 			'line-height': videoHeight
 		});
-		$("#videoPage").css({
-			'height': videoHeight,
-			'width' : (videoRatio*mainHeight) + "px"
+		$("#videoModal > div").css({
+			'line-height': videoHeight
+		});
+		$("#videoModal").css({
+			'line-height': height + "px"
 		});
 		$("#signUpForm").css({
 			'height': subHeight + 'px'
 		});
-		$("#signUpForm > div").css({
-			'line-height': subHeight + 'px'
-		});
+
+		resizeVideo();
 		// $("#signUpForm > div > input").css({
 		// 	'line-height': subHeight + 'px'
 		// });
@@ -36,10 +47,25 @@ $(document).on('ready', function() {
 
 	setWindowSize();
 
-	$("#logoPage button").click(function() {
-		$("body").animate({
-			'scrollTop':videoHeight
-		}, 500);
+	var hideVideo = function () {
+		$("#videoModal").fadeOut(200);
+		$("#modalBackground").fadeOut(500);
+	}
+
+	$("#playButton").click(function() {
+		$("#modalBackground").fadeIn(300, function() {
+			$("#videoModal").fadeIn(300);
+		});
+	});
+
+	$("#videoModal").click(function() {
+		hideVideo();
+	});
+
+	$(window).keydown(function(event) {
+		if(event.which === 27) {
+			hideVideo();
+		}
 	});
 
 	$(window).resize(function() {
