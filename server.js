6ad11b6c5a03a5a4775a06bed9d264dc8727e617
George Nishimura <thetmkay@gmail.com>
2014-03-21 16:37:59 +0000

    adding debug logs

--- server.js
+++ server.js
@@ -51,6 +51,8 @@ app.get('*', function(req,res) {
  * Start Server
  */
 
+console.log("starting server");
+
 http.createServer(app).listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
 });
